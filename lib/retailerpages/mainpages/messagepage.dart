613a7d6f64d6e8371b6e4359f149a282b215330d
Mark Choorakat <markchoorakat@gmail.com>
2024-04-13 16:50:33 +0530

    newver

--- lib/retailerpages/mainpages/messagepage.dart
+++ lib/retailerpages/mainpages/messagepage.dart
@@ -5,6 +5,55 @@ class RetMessagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              ElevatedButton(
+                child: Row(
+                  children: [
+                    Icon(Icons.request_page),
+                    SizedBox(width: 10),
+                    Text('Requests', style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                onPressed: () {
+                  // Add your action for request button tap
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              ElevatedButton(
+                child: Row(
+                  children: [
+                    Icon(Icons.message),
+                    SizedBox(width: 10),
+                    Text('Messages', style: TextStyle(fontSize: 20)),
+                  ],
+                ),
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/retailerpages/mainpages/profilepage.dart
+++ lib/retailerpages/mainpages/profilepage.dart
@@ -1,10 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:traderapp/components/bottomitems.dart';
+import 'package:traderapp/models/user.dart';
+import 'package:traderapp/retailerpages/mainpages/profileedit.dart';
 
 class RetProfilePage extends StatelessWidget {
   const RetProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/d/d7/Cristiano_Ronaldo_playing_for_Al_Nassr_FC_against_Persepolis%2C_September_2023_%28cropped%29.jpg'),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'ronaldo',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                'ID: 123456',
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfileEdit(),
+                    ),
+                  );
+                },
+                child: Text('Edit Profile'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
